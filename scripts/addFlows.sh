15337cdaf8d1230a17d9a5b63a01f880df21e14f
Assaf Muller <amuller@redhat.com>
2013-07-29 20:17:10 +0300

    Added .idea to gitignore Added default drop flow Removed taps from bridge in trafficDown Changed to 1 socket 1 core in vmUp, trafficUp

--- scripts/addFlows.sh
+++ scripts/addFlows.sh
@@ -6,3 +6,4 @@ ovs-ofctl add-flow lb "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,n
 ovs-ofctl add-flow lb "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.2/0.0.0.3,actions=output:3" 
 ovs-ofctl add-flow lb "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.3/0.0.0.3,actions=output:4"
 ovs-ofctl add-flow lb "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.1,actions=output:5"
+ovs-ofctl add-flow lb "table=0,priority=0,actions=drop"
--- scripts/vmTrafficDown.sh
+++ scripts/vmTrafficDown.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+ovs-vsctl del-port wan tap500
+ovs-vsctl del-port lb tap501
+
 tunctl -d tap500
 tunctl -d tap501
