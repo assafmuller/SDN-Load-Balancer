b2b8ab33b1c933c58311869865d52570ed751e6e
root <root@dhcp-1-3.tlv.redhat.com>
2013-09-06 11:42:37 +0300

    Moved back to addFlows.sh, addFlowsRewrite.sh and support 2 VMs

--- scripts/addFlowsRewrite.sh
+++ scripts/addFlowsRewrite.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 ovs-ofctl del-flows traffic
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.0/0.0.0.3,actions=mod_nw_dst:1.1.1.11,mod_dl_dst:02:00:00:00:00:01,output:1" 
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.1/0.0.0.3,actions=mod_nw_dst:1.1.1.12,mod_dl_dst:02:00:00:00:00:02,output:2" 
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.2/0.0.0.3,actions=mod_nw_dst:1.1.1.13,mod_dl_dst:02:00:00:00:00:03,output:3" 
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.3/0.0.0.3,actions=mod_nw_dst:1.1.1.14,mod_dl_dst:02:00:00:00:00:04,output:4"
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.11,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:5"
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.12,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:5"
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.13,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:5"
-ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.14,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:5"
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.0/0.0.0.1,actions=mod_nw_dst:1.1.1.11,mod_dl_dst:02:00:00:00:00:01,output:1" 
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,nw_src=0.0.0.1/0.0.0.1,actions=mod_nw_dst:1.1.1.12,mod_dl_dst:02:00:00:00:00:02,output:2" 
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.11,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:3"
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.12,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:3"
 ovs-ofctl add-flow traffic "table=0,priority=0,actions=drop"
---
+++ scripts/addFlowsRewriteTrimmed.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ovs-ofctl del-flows traffic
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,nw_dst=1.1.1.1,ip,actions=mod_nw_dst:1.1.1.11,mod_dl_dst:02:00:00:00:00:01,output:1" 
+ovs-ofctl add-flow traffic "table=0,idle_timeout=0,hard_timeout=0,ip,nw_src=1.1.1.11,actions=mod_nw_src:1.1.1.1,mod_dl_src:02:00:00:00:00:00,output:2"
+ovs-ofctl add-flow traffic "table=0,priority=0,actions=drop"
